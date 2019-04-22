36207347eefff926fd4e7f6315ef9bb1d5dabe41
Matt Mayfield <mdmayfield@users.noreply.github.com>
2019-04-22 16:42:14 -0500

    Experiments and improvements to writing to doc

--- loggingLetters.js
+++ loggingLetters.js
@@ -1,8 +1,11 @@
 var sentence = "I luuuuv learning software development";
 
+document.write("<p><strong>until now.</strong></p>");
+
 for (var i=0; i < sentence.length; i++) {
   if(sentence[i] !== " ") {
-    document.write(sentence[i]);
+    document.write(sentence[i] + "<br />");
   }
 }
+
 
