9692c2ecd6d8b99c394fde607df04dd466cb87eb
Matt Mayfield <mdmayfield@users.noreply.github.com>
2019-04-22 16:52:24 -0500

    Some progress on ifStatements.js

--- ifStatements.js
+++ ifStatements.js
@@ -3,7 +3,16 @@ var result = 0;
 
 document.write("<p>Before discombobulation and transmogrification, the value of <b>n</b> is " + n + " and the value of <b>result</b> is " + result + ".</p>");
 
-// TODO: everything
-
+if (Number.isInteger(n) && Number.isInteger(result)) {
+  if (n % 2) {
+    result = result + 10;
+  } else {
+    result = result - n;
+  }
+} else {
+  document.write("<p>One or both of those numbers is not an integer, so tough luck.</p>");
+  n = "unusable";
+  result = "irrelevant";
+}
 
 document.write("<p>After discombobulation and transmogrification, the value of <b>n</b> is " + n + " and the value of <b>result</b> is " + result + ".</p>");
