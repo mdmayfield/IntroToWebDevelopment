3b6d913cf7800d3093844f5a1d4f2f1dfcff6593
Matt Mayfield <mdmayfield@users.noreply.github.com>
2019-04-22 16:47:34 -0500

    Skeleton of Code Practice: if statments

---
+++ ifStatements.js
@@ -0,0 +1,9 @@
+var n = 25;
+var result = 0;
+
+document.write("<p>Before discombobulation and transmogrification, the value of <b>n</b> is " + n + " and the value of <b>result</b> is " + result + ".</p>");
+
+// TODO: everything
+
+
+document.write("<p>After discombobulation and transmogrification, the value of <b>n</b> is " + n + " and the value of <b>result</b> is " + result + ".</p>");
