c39a6f402c34293c29d8fb8733f43098d24a55e0
Matt Mayfield <mdmayfield@users.noreply.github.com>
2019-04-22 21:16:05 -0500

    Sum Of Rage array exercise - that was a typo but I'm not fixing it

---
+++ sumOfRange.js
@@ -0,0 +1,8 @@
+var bunchaNumbers = [4, 8, 15, 16, 23, 42, 2, 19, 100, 12];
+var sum = 0;
+
+for (var i = 0; i < bunchaNumbers.length; i++) {
+  sum += bunchaNumbers[i];
+}
+
+console.log("The sum of all members of bunchaNumbers is " + sum + ".");
