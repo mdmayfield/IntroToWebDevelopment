cb875821566e5fa730b86b7de38918dd56840311
Matt Mayfield <mdmayfield@users.noreply.github.com>
2019-04-23 11:22:54 -0500

    clickToAddItems.js in typo debugging process

--- clickToAddItems.js
+++ clickToAddItems.js
@@ -1,7 +1,7 @@
 function clearErrors() {
   for (var loopCounter = 0;
        loopCounter < document.forms["numberFun"].elements.length;
-       loopCounter++; {
+       loopCounter++;) {
     if (document.forms["numberFun"].elements[loopCounter]
         .parentElement.className.indexOf("has-") != -1) {
       document.forms["numberFun"].elements[loopCounter]
@@ -17,4 +17,32 @@ function resetForm() {
   document.getElementById("results").style.display = "none";
   document.getElementById("submitButton").innerText = "Submit";
   document.forms["numberFun"]["num1"].focus();
+}
+
+function validateItems() {
+  clearErrors();
+  var num1 = document.forms["numberFun"]["num1"].value;
+  var num2 = document.forms["numberFun"]["num2"].value;
+  if (num1 == "" || isNaN(num1)) {
+    alert("Num1 must be filled in with a number.");
+    document.forms["numberFun"]["num1"]
+    .parentElement.className = "form-group has-error";
+    document.forms["numberFun"]["num1"].focus();
+    return false;
+  if (num2 == "" || isNaN(num1)) {
+    alert("Num2 must be filled in with a number.");
+    document.forms["numberFun"]["num2"]
+    .parentElement.className = "form-group has-error";
+    document.forms["numberFun"]["num2"].focus();
+    return false;
+  }
+  document.getElementById("results").style.display = "block";
+  document.getElementById("submitButton").innerText = "Recalculate";
+  /* because + could be interpreted as concatenation instead of addition */
+  document.getElementById("addResult").innerText = Number(num1) + Number(num2);
+  document.getElementById("subtractResult").innerText = num1 - num2;
+  document.getElementById("multiplyResult").innerText = num1 * num2;
+  document.getElementById("divideResult").innerText = num1 / num2;
+  /* return false so form doesn't submit and we lose the results */
+  return false;
 }
