24afbd2610048a60b43bbc95b8eccd49a7c7f82a
Matt Mayfield <mdmayfield@users.noreply.github.com>
2019-04-23 10:10:22 -0500

    fix typos

--- comparingElements.js
+++ comparingElements.js
@@ -4,8 +4,8 @@ var testArray = [0, 1, 1, "1", 3, "311"];
 for (var arrayPosition = 0;
      arrayPosition < testArray.length - 1;
      arrayPosition++) {
-  var currentElement = testArray[arrayPostition];
-  var nextElement = testArray[arrayPostition + 1];
+  var currentElement = testArray[arrayPosition];
+  var nextElement = testArray[arrayPosition + 1];
 
   console.log("Testing " + currentElement + " and " + nextElement +
               "(greater than): " + (currentElement > nextElement));
