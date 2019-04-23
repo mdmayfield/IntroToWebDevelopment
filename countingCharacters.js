4bb82cb826f4bb93d29bca8e860e047b5272f252
Matt Mayfield <mdmayfield@users.noreply.github.com>
2019-04-22 19:05:06 -0500

    Add countingCharacters2

--- countingCharacters.js
+++ countingCharacters.js
@@ -2,4 +2,17 @@ function countingCharacters(stringToCount) {
   console.log(stringToCount + " has " + stringToCount.length + " characters.");
 }
 
+function countingCharacters2(stringToCount, characterToFind) {
+  var characterCount = 0;
+  for (var characterPosition = 0;
+       characterPosition < stringToCount.length;
+       characterPosition++) {
+    if (stringToCount[characterPosition] == characterToFind) {
+      characterCount++;
+    }
+  }
+  console.log("String to search in: " + stringToCount);
+  console.log("Character to find: " + characterToFind);
+  console.log("Number of times the character appears: " + characterCount);
+}
 
