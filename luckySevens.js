b65546468dc14a06270544ee646e1c0e625a73ac
Matt Mayfield <mdmayfield@users.noreply.github.com>
2019-04-23 21:06:44 -0500

    A little text manipulation to convert to a Number

--- luckySevens.js
+++ luckySevens.js
@@ -1,6 +1,10 @@
 function playLuckySevens() {
   var money = document.forms["playfield"]["startingBet"].value;
   console.log(money);
+
+  realMoney = Number(money.substring(1,money.length));
+  console.log(realMoney);
+
   return false;
 }
 
